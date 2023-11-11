2ab057d0643509de576076d9ca153105ffcda66b
Gilles Helleu <helleugilles@gmail.com>
2023-11-11 15:33:22 +0100

    change formation

--- gh/admin.py
+++ gh/admin.py
@@ -2,26 +2,30 @@ from django.contrib import admin
 from django.utils.html import format_html
 from .models import ImageProjet, Projet, Langage, Lien
 
+
 class LienInline(admin.StackedInline):
     model = Lien
     extra = 1  # Par défaut, affiche un champ vide pour ajouter un nouveau lien
 
+
 class ImageProjetInline(admin.StackedInline):
     model = ImageProjet
-    extra = 1  
+    extra = 1
 
 
 @admin.register(Projet)
 class ProjetAdmin(admin.ModelAdmin):
-    list_display = ('titre', 'soustitre', 'image_tag', 'description', 'annee')
+    list_display = ('titre', 'soustitre', 'display_order',
+                    'description', 'annee')
     list_filter = ('langages',)
+    ordering = ('display_order',)
     inlines = [LienInline, ImageProjetInline]
 
     def image_tag(self, obj):
         return format_html('<img src="{}" width="50" height="50" />', obj.image.url)
     image_tag.short_description = 'Image'
 
+
 @admin.register(Langage)
 class LangageAdmin(admin.ModelAdmin):
     pass
-
