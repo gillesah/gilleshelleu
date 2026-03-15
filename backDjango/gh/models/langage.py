4daa8ef67ab8f4bcb739b8010ddd57dcd646a112
gillesah <helleugilles@gmail.com>
2026-03-15 17:59:57 +0100

    chore: remove old backend folders

--- backDjango/gh/models/langage.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django import forms
-
-
-class Langage(models.Model):
-    nom = models.CharField(max_length=50, unique=True, blank=True, null=True,)
-
-    def __str__(self):
-        return self.nom
--- backDjango/static/js/burger.js
+++
@@ -1,8 +0,0 @@
-var burgerMenu = document.getElementById("burger-menu");
-
-var overlay = document.getElementById("menu");
-console.log("youpi");
-burgerMenu.addEventListener("click", function () {
-  this.classList.toggle("close");
-  overlay.classList.toggle("overlay");
-});
